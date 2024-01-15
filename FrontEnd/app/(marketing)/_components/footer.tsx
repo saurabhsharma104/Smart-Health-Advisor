4f4bca35f9b53933f9d9ab24acf52aeee5c7c292
saurabh sharma <saurabhsharma29520@gmail.com>
2024-01-15 22:49:53 +0530

    [Final Commit] Organized folder structure

--- FrontEnd/app/(marketing)/_components/footer.tsx
+++ FrontEnd/app/(marketing)/_components/footer.tsx
@@ -6,9 +6,9 @@ const FooterPage = () => {
         <div className="flex items-center w-full p-6 bg-background z-50 dark:bg-[#1f1f1f]">
             <Logo/>
             <div className="md:ml:auto w-full justify-between md:justify-end flex items-center gap-x-2 text-muted-foreground">
-                <Button variant="ghost" size="sm">Privacy Policy</Button>
+{/*                 <Button variant="ghost" size="sm">Privacy Policy</Button> */}
 
-                <Button variant="ghost" size="sm">Terms & Conditions</Button>
+{/*                 <Button variant="ghost" size="sm">Terms & Conditions</Button> */}
 
             </div>
         </div>
